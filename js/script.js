94f57141da3e41ee99f2afac10f720094ca3b8ad
Stas <voironlol@gmail.com>
2020-09-28 19:58:26 +0300

    First lesson

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log(3 + ' - object');
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних фильмов', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних фильмов', ''),
+      d = prompt('На сколько оцените его', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
